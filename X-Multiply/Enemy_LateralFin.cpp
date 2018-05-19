f1c7e1cc6155a7b8671914ca71219eabaed29687
RoperoIvan <ivan_ropero98@hotmail.com>
2018-05-19 16:48:13 +0200

    Lateral Fin animation's speed fixed

--- X-Multiply/Enemy_LateralFin.cpp
+++ X-Multiply/Enemy_LateralFin.cpp
@@ -7,10 +7,10 @@
 Enemy_LateralFin::Enemy_LateralFin(int x, int y) : Enemy(x, y)
 {
 	latfin.PushBack({ 41, 197, 75, 76 });
-	latfin.PushBack({ 134, 195, 66, 78 });
-	latfin.PushBack({ 218, 196, 35, 76 });
-	latfin.PushBack({ 50, 294, 67, 76 });
-	latfin.speed = 0.1f;
+	latfin.PushBack({ 134, 195, 75, 76 });
+	latfin.PushBack({ 218, 196,75, 76 });
+	latfin.PushBack({ 50, 294, 75, 76 });
+	latfin.speed = 0.08f;
 	animation = &latfin;
 	collider = App->collision->AddCollider({ 0, 0, 75, 76 }, COLLIDER_TYPE::COLLIDER_ENEMY, (Module*)App->enemies);
 	original_y = y;
